66ddf4abce7a892f1684d2002132b1087498d820
JuridicalBear14 <throwawayemail2223@gmail.com>
2021-06-14 12:05:31 -0700

    Continued adding code examples

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+
+for i in range(5):  #This loop creates the variable "i", with the starting value zero
+    print(i)        #It then goes through the code within the loop, and then increments i by one, it does this until i reaches the value we gave it (in this case: 5)
+
+#Important note: The stopping number is EXCLUSIVE, which means the program will stop before i reaches that number, in this program it only counts up to 4 because it stops short of 5
+
+#OUTPUT-> 0
+#OUTPUT-> 1
+#OUTPUT-> 2
+#OUTPUT-> 3
+#OUTPUT-> 4
---
+++ while_loops.py
@@ -0,0 +1,12 @@
+
+number = 10  #A variable used for our conditional later
+
+while (number > 5):  #This loop checks that number is greater than 5, if number is it runs through whatever is in the loop and then checks again
+    print(number)
+    number = number - 1  #Inside the loop it just prints number and then subracts one from it
+
+#OUTPUT-> 10
+#OUTPUT-> 9
+#OUTPUT-> 8
+#OUTPUT-> 7
+#OUTPUT-> 6

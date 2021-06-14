85f30c83afad26e188081a2b8b04cda735589b2e
JuridicalBear14 <throwawayemail2223@gmail.com>
2021-06-14 15:38:55 -0700

    Added new lessons

---
+++ arguments.py
@@ -0,0 +1,8 @@
+
+def add_five(number):  #Creates a function named "add_five" that takes the argument: number
+
+    return number + 5  #This line returns our argument plus five
+
+print(add_five(10))  #This prints out the result of calling our function with the argument 10
+
+#OUTPUT-> 15
